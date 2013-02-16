ce7cb9d67b5b2cd4ab5e7bd1d35ef43b75b9aefb
SteinerHunter <steinerhunter@gmail.com>
2013-02-16 09:55:29 +0200

    Now all forms, including signin, are fully responsive

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  respond_to :html, :js
 
   def new
     render "new.html.erb", :layout => false
@@ -6,12 +7,10 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    @session = user && user.authenticate(params[:session][:password])
+    if @session
       sign_in user
-      redirect_back_or root_url
-    else
-      flash.now[:error] = 'Invalid email/password combination'
-      render 'new'
+      respond_with(user, :location => root_path)
     end
   end
 

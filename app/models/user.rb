45a9be740aa25b9d14f88b8f4919b5c93926a5d4
SteinerHunter <steinerhunter@gmail.com>
2013-02-28 17:22:31 +0200

    Successfully added mailboxer

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,6 +47,14 @@ class User < ActiveRecord::Base
     Matepost.all
   end
 
+  def mailboxer_email(object)
+    #Check if an email should be sent for that object
+    #if true
+    return "define_email@on_your.model"
+    #if false
+    #return nil
+  end
+
   private
 
     def create_remember_token

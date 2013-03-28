f65a5b6cc132d7055215fb41087da7e750ce8db4
SteinerHunter <steinerhunter@gmail.com>
2013-03-28 22:21:27 +0200

    Did some face-lifting to Conversation Messages

--- app/models/requestpost.rb
+++ app/models/requestpost.rb
@@ -6,7 +6,6 @@ class Requestpost < ActiveRecord::Base
   validates :requested_item, :presence => { :message => "OOPS! Looks like you didn't tell us what you wanted..."}
   validates :requested_item, :length =>  { maximum: 25, :message => "OOPS!  Looks like your request's title is a bit too long..." }
   validates :give_giftcards, :presence => { :message => "OOPS! Surely you'd like to give something back, right...?"}
-  validates :giftCardsPlace, :presence => true, :length => {minimum: 2}
   validates :content, :presence => { :message => "OOPS! Looks like you didn't tell us a bit more about what you wanted..."}
   validates :content, :length => { maximum: 140 , :message => "OOPS! Looks like your description is a little too long..."}
 

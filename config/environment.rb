8a8900252bb9306d0aa3d74756882e7e047c0a80
SteinerHunter <steinerhunter@gmail.com>
2013-03-29 14:19:56 +0300

    Changed Zoho to SendGrid

--- config/environment.rb
+++ config/environment.rb
@@ -9,14 +9,12 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.smtp_settings = {
-    :address            => "smtp.zoho.com",
-    :port                   => 465,
+    :address            => "smtp.sendgrid.net",
+    :port                   => 587,
     :domain              => "beamate.com",
-    :user_name       => "firstmate@beamate.com",
-    :password         => "thiswillbeourflagship",
-    :authentication       => :login,
-    :ssl                  => true,
-    :tls                  => true,
+    :user_name       =>  ENV['SENDGRID_USERNAME'],
+    :password         =>  ENV['SENDGRID_PASSWORD'],
+    :authentication       => :plain,
     :enable_starttls_auto => true
 }
 

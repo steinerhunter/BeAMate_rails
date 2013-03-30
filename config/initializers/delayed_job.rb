8cd9d4cb6bff2d9ef7e4effe091bfe5561436a2b
SteinerHunter <steinerhunter@gmail.com>
2013-03-30 18:45:38 +0300

    Changed delayed_job.rb again

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -4,9 +4,12 @@ Delayed::Worker.sleep_delay = 2
 Delayed::Worker.max_attempts = 5
 Delayed::Worker.max_run_time = 1.hour
 Delayed::Worker.delay_jobs = !Rails.env.test?
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'dj.log'))
 
 if Rails.env.development?
+  system "RAILS_ENV=development #{Rails.root.join('script','delayed_job')} stop"
   system "RAILS_ENV=development #{Rails.root.join('script','delayed_job')} start"
 elsif Rails.env.production?
+  system "RAILS_ENV=production #{Rails.root.join('script','delayed_job')} stop"
   system "RAILS_ENV=production #{Rails.root.join('script','delayed_job')} start"
 end

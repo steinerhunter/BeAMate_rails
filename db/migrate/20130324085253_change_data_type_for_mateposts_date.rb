0f3fa56f38c2c98211b04bc22b04252f962efe5a
SteinerHunter <steinerhunter@gmail.com>
2013-03-30 14:59:11 +0300

    Changed some DB stuff again

--- db/migrate/20130324085253_change_data_type_for_mateposts_date.rb
+++ db/migrate/20130324085253_change_data_type_for_mateposts_date.rb
@@ -1,15 +1,16 @@
 class ChangeDataTypeForMatepostsDate < ActiveRecord::Migration
   def up
-    remove_column :departing_on, :string
-    add_column        :departing_on, :datetime
-    remove_column :returning_on, :string
-    add_column        :returning_on, :datetime
+    remove_column :mateposts, :departing_on
+    add_column        :mateposts, :departing_on, :datetime
+    remove_column :mateposts, :returning_on
+    add_column        :mateposts, :departing_on, :datetime
   end
 
   def down
-    remove_column :departing_on, :datetime
-    add_column        :departing_on, :string
-    remove_column :returning_on, :datetime
-    add_column        :returning_on, :string
+    remove_column :mateposts, :departing_on
+    add_column        :mateposts, :departing_on, :string
+    remove_column :mateposts, :returning_on
+    add_column        :mateposts, :departing_on, :string
+    end
   end
 end

3a3695ff5547da5e8d6dcdd776bece111f372786
SteinerHunter <steinerhunter@gmail.com>
2013-03-30 15:01:44 +0300

    Syntax error fixed

--- db/migrate/20130324085253_change_data_type_for_mateposts_date.rb
+++ db/migrate/20130324085253_change_data_type_for_mateposts_date.rb
@@ -11,6 +11,5 @@ class ChangeDataTypeForMatepostsDate < ActiveRecord::Migration
     add_column        :mateposts, :departing_on, :string
     remove_column :mateposts, :returning_on
     add_column        :mateposts, :departing_on, :string
-    end
   end
 end

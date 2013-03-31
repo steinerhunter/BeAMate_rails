818413956c987fc83846729f53318789ecd6d854
SteinerHunter <steinerhunter@gmail.com>
2013-03-31 11:10:01 +0300

    Change attr_accessible to attr_accessor in Feedback model

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,5 +1,5 @@
 class Feedback
-  attr_accessible :name, :email, :body
+  attr_accessor :name, :email, :body
 
   validates :name, :presence => { :message => "OOPS! Looks like you didn't tell us your name..."}
   validates :email, :presence => { :message => "OOPS! We're going to need your email address..."}
